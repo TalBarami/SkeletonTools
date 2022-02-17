5a5ca3e2c4881f52e11300c53a285122b0fe7fe1
Tal Baramit <baramit@post.bgu.ac.il>
2022-02-17 14:23:48 +0200

    - OpenPoseInitializer can convert video to PoseC3D input. - Added visualization code.

--- src/skeleton_tools/pipe_components/openpose_initializer.py
+++ src/skeleton_tools/pipe_components/openpose_initializer.py
@@ -11,6 +11,8 @@ from copy import deepcopy
 from PIL import Image
 from tqdm import tqdm
 
+from skeleton_tools.openpose_layouts.body import BODY_25_LAYOUT, COCO_LAYOUT
+from skeleton_tools.openpose_layouts.graph_layout import convert_layout
 from skeleton_tools.pipe_components.yolo_v5_child_detector import ChildDetector
 from skeleton_tools.utils.constants import LENGTH, JSON_SOURCES, EPSILON
 from skeleton_tools.utils.skeleton_utils import normalize_json
@@ -89,58 +91,6 @@ class OpenposeInitializer:
                            'skeleton': skeletons})
         return result
 
-    # def openpose_to_numpy(self, openpose_dir):
-    #     file_names = [path.join(openpose_dir, f) for f in os.listdir(openpose_dir) if path.isfile(path.join(openpose_dir, f)) and f.endswith('json')]
-    #
-    #     def append_info(pose, score, p, key):
-    #         pose = pose + list(zip(np.round(p[key][::3], 8), np.round(p[key][1::3], 8)))
-    #         score = np.concatenate((score, np.round(p[key][2::3], 8)))
-    #         return np.array(pose), score
-    #
-    #     T = len(file_names)
-    #     kp = np.zeros((T, self.num_person_in, self.V, self.C - 1))
-    #     scores = np.zeros((T, self.num_person_in, self.V))
-    #     pids = np.zeros((T, self.num_person_in))
-    #
-    #     for i, file in tqdm(enumerate(file_names), ascii=True, desc='Openpose to numpy'):
-    #         frame_info = read_json(file)
-    #         people = frame_info['people']
-    #
-    #         # frame_kp = []
-    #         # frame_scores = []
-    #         # frame_pids = []
-    #         for pidx, p in enumerate(people):
-    #             if pidx > self.num_person_in:
-    #                 raise IndexError("Reached maximum number of people")
-    #             pose, score = append_info([], np.array([]), p, 'pose_keypoints_2d')
-    #             # pose = list(zip(np.round(p['pose_keypoints_2d'][::3], 8), np.round(p['pose_keypoints_2d'][1::3], 8)))
-    #             # score = np.round(p['pose_keypoints_2d'][2::3], 8)
-    #             if self.layout.face:
-    #                 pose, score = append_info(pose, score, p, 'face_keypoints_2d')
-    #                 # pose += list(zip(np.round(p['face_keypoints_2d'][::3], 8), np.round(p['face_keypoints_2d'][1::3], 8)))
-    #                 # score = np.concatenate((score, np.round(p['face_keypoints_2d'][2::3], 8)))
-    #             if self.layout.hand:
-    #                 pose, score = append_info(pose, score, p, 'hand_left_keypoints_2d')
-    #                 pose, score = append_info(pose, score, p, 'hand_right_keypoints_2d')
-    #                 # pose += list(zip(np.round(p['hand_left_keypoints_2d'][::3], 8), np.round(p['hand_left_keypoints_2d'][1::3], 8)))
-    #                 # score = np.concatenate((score, np.round(p['hand_left_keypoints_2d'][2::3], 8)))
-    #                 # pose += list(zip(np.round(p['hand_right_keypoints_2d'][::3], 8), np.round(p['hand_right_keypoints_2d'][1::3], 8)))
-    #                 # score = np.concatenate((score, np.round(p['hand_right_keypoints_2d'][2::3], 8)))
-    #             kp[i, pidx] = pose
-    #             scores[i, pidx] = score
-    #             pids[i, pidx] = p['person_id'] if p['person_id'] != [-1] else pidx
-    #     return kp, scores, pids
-
-    # def collect_skeleton(self, skeleton_folder, dst_folder, resolution, tracking=True):  # TODO: Remove tracking, remove labeling
-    #     skeleton = self.openpose_to_json(skeleton_folder)
-    # skeleton = self.normalize_pose(skeleton, resolution)
-    # if tracking:
-    #     skeleton = track(skeleton, skeleton_folder, resolution, self.layout)
-    # result = {'data': self.normalize_pose(skeleton, resolution),
-    #           'label': label_name,
-    #           'label_index': int(label_index)}
-    # tools.write_json(skeleton, path.join(dst_folder, f'{path.basename(skeleton_folder)}.json'))
-
     def prepare_skeleton(self, src_path, result_skeleton_dir=None, source_type=SkeletonSource.VIDEO, out_name=None):
         basename = path.basename(src_path)
         basename_no_ext = path.splitext(basename)[0] if source_type == SkeletonSource.VIDEO else basename
@@ -168,8 +118,10 @@ class OpenposeInitializer:
         finally:
             shutil.rmtree(openpose_output_path)
 
-    def normalize(self, pose_json, resolution):
-        return normalize_json(pose_json, resolution)
+    def normalize(self, skeleton, centralize):
+        new_skeleton = skeleton.copy()
+        new_skeleton['data'] = normalize_json(skeleton['data'], skeleton['resolution'], centralize=centralize)
+        return new_skeleton
 
     def denormalize(self, data_numpy, resolution):
         x = data_numpy.copy()
@@ -183,7 +135,7 @@ class OpenposeInitializer:
 
     def to_numpy(self, skeleton):
         data_numpy = np.zeros((self.C, self.T, self.V, self.num_person_in))
-        for i, frame_info in tqdm(enumerate(skeleton), ascii=True, desc='To numpy'):
+        for i, frame_info in tqdm(enumerate(skeleton['data']), ascii=True, desc='To numpy'):
             if i == self.T:
                 break
             for m, skeleton_info in enumerate(frame_info["skeleton"]):  # TODO: case when id > num_person_in, but len(skeleton) < num_person_in
@@ -201,3 +153,30 @@ class OpenposeInitializer:
         for t, s in enumerate(sort_index):
             data_numpy[:, t, :, :] = data_numpy[:, t, :, s].transpose((1, 2, 0))
         return data_numpy[:, :, :, 0:self.num_person_out]
+
+    def _to_posec3d_numpy(self, skeleton_data, in_channels=2, max_people=1, src_layout=BODY_25_LAYOUT, dst_layout=COCO_LAYOUT):
+        kps = np.zeros((max_people, len(skeleton_data), len(dst_layout), in_channels))
+        scores = np.zeros((max_people, len(skeleton_data), len(dst_layout)))
+
+        for i, frame_info in enumerate(skeleton_data):
+            for j, skeleton in enumerate(frame_info['skeleton']):
+                kp, s = np.array([skeleton['pose'][::2], skeleton['pose'][1::2]]).T, np.array(skeleton['pose_score'])
+                kps[j, i, :, :] = convert_layout(kp, src_layout, dst_layout)
+                scores[j, i, :] = convert_layout(s, src_layout, dst_layout)
+        return kps, scores
+
+    def to_poseC3D(self, json_file, label=None, label_index=None):
+        kp, s = self._to_posec3d_numpy(json_file['data'])
+        result = {
+            'keypoint': kp,
+            'keypoint_score': s,
+            'frame_dir': json_file['name'],
+            'img_shape': json_file['resolution'],
+            'original_shape': json_file['resolution'],
+            'fps': json_file['fps'],
+            'total_frames': len(json_file['data']),
+        }
+        if label is not None and label_index is not None:
+            result['label_name'] = label
+            result['label'] = label_index
+        return result
--- src/skeleton_tools/skeleton_visualization/plotter.py
+++ src/skeleton_tools/skeleton_visualization/plotter.py
@@ -6,10 +6,12 @@ import numpy as np
 # import torch.fft as fft
 from scipy.fftpack import fft
 import pandas as pd
-
+import seaborn as sns
+from sklearn import metrics
 
 from skeleton_tools.openpose_layouts.body import BODY_25_LAYOUT
-from skeleton_tools.utils.tools import read_json
+from skeleton_tools.utils.constants import REAL_DATA_MOVEMENTS
+from skeleton_tools.utils.tools import read_json, get_video_properties
 
 
 def plot_fft(data_numpy, title, filename):
@@ -49,35 +51,146 @@ def plot_fft(data_numpy, title, filename):
     # plt.show()
 
 
-def plot_layers():
-    return
+def bar_plot_lenghts(df_path):
+    fig, ax = plt.subplots()
+    df = pd.read_csv(df_path)
+    df['length'] = df['end_time'] - df['start_time']
+    classes, lengths = list(zip(*list(df.groupby('movement')['length'])))
+    lengths = [l.to_numpy() for l in lengths]
+    groups = list(zip([(0, 2), (2, 5), (5, 8), (8, np.inf)], ['#1D2F6F', '#8390FA', '#6EAF46', '#FAC748']))
+    prev = np.zeros(len(classes))
+    for (min_len, max_len), color in groups:
+        counts = [np.count_nonzero(l[(min_len < l) & (l <= max_len)]) for l in lengths]
+        ax.barh(classes, counts, color=color, left=prev)
+        prev += counts
+    legend = [f'{min_len}s - {max_len}s' for (min_len, max_len), _ in groups]
+    legend[-1] = f'{groups[-1][0][0]}s $\leq$'
+    plt.legend(legend, loc='lower right', ncol=4)
+    plt.xlabel('Number of videos')
+    plt.ylabel('Class')
+    plt.savefig('resources/figs/videos_lengths.png', bbox_inches='tight')
+    plt.show()
 
+
+def bar_plot_unique_children(df_path):
+    fig, ax = plt.subplots()
+    df = pd.read_csv(df_path)
+    df['ckey'] = df['video'].apply(lambda s: s.split('_')[0])
+    gp = df.groupby(['movement'])['ckey'].nunique()
+    ax.barh(gp.index, gp.values / df['ckey'].nunique() * 100)
+    for p in ax.patches:
+        width = p.get_width()
+        height = p.get_height()
+        x, y = p.get_xy()
+        ax.annotate(f'{int(width)}%', (x + width + 4, y + height * 0.25), ha='center')
+    ax.set_xlim(0, 80)
+    ax.set_xlabel('Percentage of unique children')
+    ax.set_ylabel('Class')
+    plt.savefig('resources/figs/unique_children.png', bbox_inches='tight')
+    plt.show()
+
+classmap = {0: 'Hand flapping',
+                1: 'Tapping',
+                2: 'Clapping',
+                3: 'Fingers',
+                4: 'Body rocking',
+                5: 'Tremor',
+                6: 'Spinning in circle',
+                7: 'Toe walking',
+                8: 'Back and forth',
+                9: 'Head movement',
+                10: 'Playing with object',
+                11: 'Jumping in place'}
+classmap = list(classmap.values())
+
+def plot_scores_heatmap(preds_df):
+    v = []
+    for i in range(len(classmap)):
+        df = preds_df[preds_df['y'] == i][preds_df.columns[1:]]
+        v.append(df.mean().to_numpy())
+    v = np.round(np.array(v), 3)
+    df_cm = pd.DataFrame(v, index=classmap, columns=classmap)
+    plt.figure(figsize=(10, 7))
+    ax = sns.heatmap(df_cm, annot=True)
+    ax.figure.tight_layout()
+    plt.savefig(r'resources/figs/heatmap.png')
+    plt.show()
+
+def plot_conf_matrix(preds_df, norm=False):
+    y_hat = preds_df['y']
+    y_pred = np.argmax(preds_df[preds_df.columns[1:]].to_numpy(), axis=1)
+    cm = metrics.confusion_matrix(y_hat, y_pred)
+    if norm:
+        counts = preds_df.groupby('y')['0'].count().to_numpy()
+        cm = np.round(cm / counts, 3)
+    df_cm = pd.DataFrame(cm, index=classmap, columns=classmap)
+    plt.figure(figsize=(10, 7))
+    ax = sns.heatmap(df_cm, annot=True, fmt='g')
+    ax.figure.tight_layout()
+    plt.savefig(r'resources/figs/confusion_matrix.png')
+    plt.show()
+
+import cv2
 if __name__ == '__main__':
-    skeletons_root = r'D:\datasets\autism_center\skeletons_filtered\data'
-    videos_root = r'D:\datasets\autism_center\segmented_videos'
+    r = r'F:\ALOTAF\2021- All\7'
 
-    files = [path.splitext(f)[0] for f in os.listdir(videos_root) if f.endswith('.avi')]
-    files = [f for i, f in enumerate(files) if f'{f}.json' in set(os.listdir(skeletons_root))]
-    names = []
+    cams = [path.join(r, f) for f in os.listdir(r) if path.isdir(path.join(r, f))]
+    out = {}
+    for cam in cams:
+        print(f'Displaying for camera: {path.basename(cam)}')
+        c = len(os.listdir(cam))
+        results = []
+        for file in os.listdir(cam):
+            res, fps, length = get_video_properties(path.join(cam, file))
+            if fps != np.inf:
+                results.append(file)
+            print(file, fps)
+        out[path.basename(cam)] = [len(results), c, results]
+    print(1)
 
+    df = pd.read_csv(r'E:\mmaction2\test.csv')
+    label = df['y']
+    predict = np.argmax(df[df.columns[1:]].to_numpy(), axis=1)
+    result = {s: ((predict == i) & (label == i)).sum() / (label == i).sum() for i, s in enumerate(REAL_DATA_MOVEMENTS)}
+    for k, v in result.items():
+        print(f'{k} & {np.round(v, 2)}\\% \\\\')
+    plot_conf_matrix(df, norm=True)
+    plot_scores_heatmap(df)
 
-
-
-    for label in ['Hand flapping', 'Tapping', 'Clapping', 'Body rocking',
-                  'Tremor', 'Toe walking', 'Head movement',
-                  'Playing with object', 'Jumping in place', 'NoAction']:
-        sub = [f for f in files if label in f]
-        for filename in sub:
-            data = read_json(path.join(skeletons_root, f'{filename}.json'))['data']
-            if len(data) < 40:
-                print(f'Video is too short: {filename}')
-                continue
-            np_result = np.zeros((3, 25, len(data)))
-            for frame_info in data:
-                if frame_info['skeleton']:
-                    skel = frame_info['skeleton'][0]
-                    np_result[0, :, frame_info['frame_index']] = skel['pose'][0::2]
-                    np_result[1, :, frame_info['frame_index']] = skel['pose'][1::2]
-                    np_result[2, :, frame_info['frame_index']] = skel['pose_score']
-            plot_fft(np_result, label, f'{filename}')
-        break
+    # vinfos = []
+    # for root, dirs, files in os.walk(r'D:\datasets\tagging_hadas&dor\raw_videos'):
+    #     for f in files:
+    #         vinfos.append(get_video_properties(path.join(root, f)))
+    # resolution = set([v[0] for v in vinfos])
+    # fps = np.unique([v[1] for v in vinfos])
+    # df_path = r'C:\Users\owner\PycharmProjects\RepetitiveMotionRecognition\resources\labels_complete_updated.csv'
+    # bar_plot_lenghts(df_path)
+    # bar_plot_unique_children(df_path)
+    # skeletons_root = r'D:\datasets\autism_center\skeletons_filtered\data'
+    # videos_root = r'D:\datasets\autism_center\segmented_videos'
+    #
+    # files = [path.splitext(f)[0] for f in os.listdir(videos_root) if f.endswith('.avi')]
+    # files = [f for i, f in enumerate(files) if f'{f}.json' in set(os.listdir(skeletons_root))]
+    # names = []
+    #
+    #
+    #
+    #
+    # for label in ['Hand flapping', 'Tapping', 'Clapping', 'Body rocking',
+    #               'Tremor', 'Toe walking', 'Head movement',
+    #               'Playing with object', 'Jumping in place', 'NoAction']:
+    #     sub = [f for f in files if label in f]
+    #     for filename in sub:
+    #         data = read_json(path.join(skeletons_root, f'{filename}.json'))['data']
+    #         if len(data) < 40:
+    #             print(f'Video is too short: {filename}')
+    #             continue
+    #         np_result = np.zeros((3, 25, len(data)))
+    #         for frame_info in data:
+    #             if frame_info['skeleton']:
+    #                 skel = frame_info['skeleton'][0]
+    #                 np_result[0, :, frame_info['frame_index']] = skel['pose'][0::2]
+    #                 np_result[1, :, frame_info['frame_index']] = skel['pose'][1::2]
+    #                 np_result[2, :, frame_info['frame_index']] = skel['pose_score']
+    #         plot_fft(np_result, label, f'{filename}')
+    #     break
