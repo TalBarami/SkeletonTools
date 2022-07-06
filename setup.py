c0d901ce57e93e1cf02aa3d160adba2c13190ffe
Tal Baramit <baramit@post.bgu.ac.il>
2022-07-06 19:26:11 +0300

    src folder removed

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(
     name='SkeletonTools',
     version='1.0',
-    packages=['src', 'src.skeleton_tools', 'src.skeleton_tools.utils', 'src.skeleton_tools.datasets', 'src.skeleton_tools.pipe_components', 'src.skeleton_tools.openpose_layouts', 'src.skeleton_tools.skeleton_visualization'],
+    packages=['skeleton_tools', 'skeleton_tools.utils', 'skeleton_tools.datasets', 'skeleton_tools.pipe_components', 'skeleton_tools.openpose_layouts', 'skeleton_tools.skeleton_visualization'],
     url='https://github.com/TalBarami/SkeletonTools',
     license='',
     author='TalBarami',
     author_email='talbaramii@gmail.com',
-    description='General code for skeleton data manipulation.'
+    description='Utilities for skeletons & videos'
 )
