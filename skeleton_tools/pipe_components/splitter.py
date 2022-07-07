08f5ca18ff831c173ec1836aff87d013e0bafd5c
Tal Baramit <baramit@post.bgu.ac.il>
2022-07-07 17:30:29 +0300

    Splitter dataset bug (not supporting vids < seq_length) fixed.

--- skeleton_tools/pipe_components/splitter.py
+++ skeleton_tools/pipe_components/splitter.py
@@ -3,17 +3,18 @@ from torch.utils.data import Dataset
 from tqdm import tqdm
 from os import path
 
-from skeleton_tools.utils.constants import LENGTH, STEP_SIZE
+from skeleton_tools.utils.constants import LENGTH, STEP_SIZE, MIN_LENGTH
 
 
 class Splitter(Dataset):
-    def __init__(self, skeleton, sequence_length=LENGTH, step_size=STEP_SIZE):
+    def __init__(self, skeleton, sequence_length=LENGTH, step_size=STEP_SIZE, min_length=MIN_LENGTH):
         self.skeleton = skeleton
         self.sequence_length = sequence_length
         self.step_size = step_size
+        self.min_length = min_length
         self.N, self.T, self.J, self.C = self.skeleton['keypoint'].shape
 
-        self.intervals = [(x, min(x+self.sequence_length, self.T)) for x in range(0, self.T - self.sequence_length + self.step_size, self.step_size)]
+        self.intervals = [(x, min(x+self.sequence_length, self.T)) for x in range(0, max(self.T, self.T - self.sequence_length + self.step_size), self.step_size) if (min(x+self.sequence_length, self.T) - x) >= self.min_length]
         self.template = {k: v for k, v in self.skeleton.items() if k not in ['keypoint', 'keypoint_score', 'total_frames', 'frame_dir']}
         self.template['basename'] = self.skeleton['frame_dir']
 
