418b152925fdfdd79d6b446843ba029fe4c1c19b
Tal <baramit@post.bgu.ac.il>
2022-09-27 15:08:35 +0300

    - Pyfeat face layout

--- skeleton_tools/skeleton_visualization/pyfeat_visualizer.py
+++ skeleton_tools/skeleton_visualization/pyfeat_visualizer.py
@@ -1,15 +1,22 @@
 from skeleton_tools.skeleton_visualization.draw_utils import draw_bbox, blur_area
+from skeleton_tools.skeleton_visualization.skeleton_visualizer import SkeletonVisualizer
+from skeleton_tools.utils.tools import get_video_properties
 
 
-class PyfeatVisualizer:
-    def __init__(self, blur_face):
-        self.blur_face = blur_face
+class PyfeatVisualizer(SkeletonVisualizer):
+    def __init__(self, graph_layout, blur_face):
+        super().__init__(graph_layout, blur_face=blur_face)
 
-    def draw_facebox(self, frame, faces):
-        for _, face in faces.iterrows():
-            bbox = face[['FaceRectX', 'FaceRectY', 'FaceRectWidth', 'FaceRectHeight']].astype(int).values.reshape(2, 2)
-            if self.blur_face:
-                frame = blur_area(frame, bbox[0], bbox[1].max())
-            color = (255, 0, 0) if face['is_child'] else (0, 0, 255)
-            draw_bbox(frame, bbox, color)
-        return frame
+    # def draw_facebox(self, frame, faces):
+    #     for _, face in faces.iterrows():
+    #         bbox = face[['FaceRectX', 'FaceRectY', 'FaceRectWidth', 'FaceRectHeight']].astype(int).values.reshape(2, 2)
+    #         if self.blur_face:
+    #             frame = blur_area(frame, bbox[0], bbox[1].max())
+    #         color = (255, 0, 0) if face['is_child'] else (0, 0, 255)
+    #         draw_bbox(frame, bbox, color)
+    #     return frame
+
+    def get_video_info(self, video_path, pyfeat_data):
+        raise NotImplementedError('TBI')
+        # resolution, fps, frame_count, length = get_video_properties(video_path)
+        # return fps, length, resolution, kp, c, pids, child_ids, detections, child_bbox, adjust
